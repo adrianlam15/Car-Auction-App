8602757e1d0b32c4584077106b0079144f35fd00
adrianlam15 <93681064+adrianlam15@users.noreply.github.com>
2023-02-06 12:47:25 -0800

    switched branches for testing main. Branch contains much simpler class hierarchy

--- src/main/model/Car.java
+++ src/main/model/Car.java
@@ -1,29 +1,15 @@
 package model;
 
-// Represents a car with make, model, condition, colour, transmission, drive type, mileage, and year made
 public class Car {
-    private String make;            // make of the car
-    private String model;           // model of the car
-    private String condition;       // condition of the car
-    private String colour;          // colour of the car
-    private String transmission;    // transmission of the car
-    private String driveType;       // drive type of the car
-    private int mileage;            // mileage of the car
-    private int yearMade;           // year the car was made
-
-    // REQUIRES: make has a non-zero length, model has a non-zero length, condition has a non-zero length,
-    //           colour has a non-zero length, transmission has a non-zero length, driveType has a non-zero length,
-    //           mileage >= 0, yearMade >= 0.
-    public Car() {
-        this.make = "";
-        this.model = "";
-        this.condition = "";
-        this.colour = "";
-        this.transmission = "";
-        this.driveType = "";
-        this.mileage = 0;
-        this.yearMade = 0;
-}
+    private String make;
+    private String model;
+    private String colour;
+    private String transmission;
+    private String driveType;
+    private String condition;
+    private int year;
+    private int price;
+    private int mileage;
 
     public void setMake(String make) {
         this.make = make;
@@ -31,10 +17,6 @@ public class Car {
 
     public void setModel(String model) {
         this.model = model;
-    }
-
-    public void setCondition(String condition) {
-        this.condition = condition;
     }
 
     public void setColour(String colour) {
@@ -49,51 +31,55 @@ public class Car {
         this.driveType = driveType;
     }
 
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     public void setMileage(int mileage) {
         this.mileage = mileage;
     }
 
-    public void setYearMade(int yearMade) {
-        this.yearMade = yearMade;
-    }
-
-    // EFFECTS: returns make of the car
     public String getMake() {
-        return this.make;
+        return make;
     }
 
-    // EFFECTS: returns model of the car
     public String getModel() {
-        return this.model;
+        return model;
     }
 
-    // EFFECTS: returns condition of the car
-    public String getCondition() {
-        return this.condition;
-    }
-
-    // EFFECTS: returns colour of the car
     public String getColour() {
-        return this.colour;
+        return colour;
     }
 
-    // EFFECTS: returns transmission of the car
     public String getTransmission() {
-        return this.transmission;
+        return transmission;
     }
 
-    // EFFECTS: returns drive type of the car
     public String getDriveType() {
-        return this.driveType;
+        return driveType;
     }
 
-    // EFFECTS: returns mileage of the car
+    public String getCondition() {
+        return condition;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
     public int getMileage() {
-        return this.mileage;
-    }
-
-    // EFFECTS: returns year the car was made
-    public int getYearMade() {
-        return this.yearMade;
+        return mileage;
     }
 }
--- src/main/ui/AuctionApp.java
+++ src/main/ui/AuctionApp.java
@@ -1,163 +1,81 @@
 package ui;
 
-import model.Car;
-import model.ListOfListing;
-import model.Listing;
 import model.User;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 // Represents the Car Auction App User Interface
 public class AuctionApp {
-    Scanner input;
-    boolean loggedIn;
-    User user = null;
-    ListOfListing listings = null;
+    private Scanner input;
+    private User user;
+    private boolean loggedIn;
+    private boolean keepGoing = true;
+    private boolean inListingView = false;
+    private boolean viewBids = false;
 
     public AuctionApp() {
-        runApp();
+        runAuctionApp();
     }
 
-    private void runApp() {
-        boolean keepGoing = true;
-        String command = null;
-
-        init();
-
-        while (keepGoing) {
-            displayMenu(loggedIn);
-            System.out.println(loggedIn);
-            command = input.next();
-            if (!loggedIn) {
-                while (!command.equals("1") && !command.equals("2") && !command.equals("3")) {
-                    System.out.println("Please enter a valid command.");
-                    command = input.next();
-                }
-                if (command.equals("3")) {
-                    keepGoing = false;
-                } else {
-                    processCommand(command);
-                }
-            } else {
-                while (!command.equals("1") && !command.equals("2") && !command.equals("3") && !command.equals("4")
-                        && !command.equals("5") && !command.equals("6") && !command.equals("7")) {
-                    System.out.println("Please enter a valid command.");
-                    command = input.next();
-                }
-                if (command.equals("6")) {
-                    loggedIn = false;
-                    user = null;
-                } else if (command.equals("7")) {
-                    keepGoing = false;
-                } else {
-                    processCommand(command);
-                }
-            }
-        }
-        System.out.println("Goodbye!");
-    }
-
-    private void init() {
+    public void runAuctionApp() {
         input = new Scanner(System.in);
         input.useDelimiter("\n");
+        loggedIn = false;
+        user = new User();
+        String command = null;
+        while (keepGoing) {
+            System.out.println(loggedIn);
+            displayMenu(loggedIn);
+            command = input.next();
+            processCommand(command);
+        };
     }
 
     private void displayMenu(boolean loggedIn) {
         if (!loggedIn) {
-            System.out.println("Welcome to the Car Auction App!");
-            System.out.println("Please select from the following options (use numbers):");
-            System.out.println("\t1. Create a new account");
-            System.out.println("\t2. Log in to an existing account");
-            System.out.println("\t3. Exit");
+            System.out.println("\nWelcome to the Car Auction App!");
+            System.out.println("Use numbers to select choices:");
+            System.out.println("\t1. Create an account.");
+            System.out.println("\t2. Login.");
+            System.out.println("\t3. Exit.");
         } else {
-            System.out.println("Welcome to the Car Auction App!");
-            System.out.println("Please select from the following options (use numbers):");
-            System.out.println("\t1. Create a new listing");
-            System.out.println("\t2. View all listings");
-            System.out.println("\t3. View saved listings");
-            System.out.println("\t4. View your listings");
-            System.out.println("\t5. View all bids");
-            System.out.println("\t6. Logout");
-            System.out.println("\t7. Exit");
+            System.out.println("\nMain menu");
+            System.out.println("Use numbers to select choices:");
+            System.out.println("\t1. Create a listing.");
+            System.out.println("\t4. Place a bid.");
+            System.out.println("\t2. View listings.");
+            System.out.println("\t3. View current bids.");
         }
     }
 
     private void processCommand(String command) {
         if (!loggedIn) {
-            if (command.equals("1")) {
-                createAccount();
+            if (command.equals("3")) {
+                System.out.println("Goodbye!");
+                keepGoing = false;
             } else if (command.equals("2")) {
-                login();
+                System.out.println("Username:");
+                String usr = input.next();
+                System.out.println("Password:");
+                String pwd = input.next();
+                loggedIn = user.login(usr, pwd);
+            } else {
+                System.out.println("Enter a username:");
+                String usr = input.next();
+                System.out.println("Enter a password:");
+                String pwd = input.next();
+                System.out.println("Re-enter your password:");
+                String checkPwd = input.next();
+                if (user.createUser(usr, pwd, checkPwd)) {
+                    System.out.println("Account created successfully.");
+                    loggedIn = user.login(usr, pwd);
+                } else {
+                    System.out.println("Passwords don't match.");
+                    processCommand("1");
+                }
             }
-        }
-        else {
-            if (command.equals("1")) {
-                createListing();
-            } else if (command.equals("2")) {
-                viewAllListings();
-            } else if (command.equals("3")) {
-                viewSavedListings();
-            } else if (command.equals("4")) {
-                viewYourListings();
-            } else if (command.equals("5")) {
-                viewAllBids();
-            } else if (command.equals("6")) {
-                loggedIn = false;
-                System.out.println("You have been logged out.");
-            }
-        }
-    }
-
-    private void createAccount() {
-        System.out.println("username:");
-        String username = input.next();
-        System.out.println("password:");
-        String password = input.next();
-        System.out.println("Your username is:");
-        System.out.println(username);
-        System.out.println("and your password is:");
-        System.out.println(password);
-        System.out.println("Is this correct?");
-        System.out.println("Y/N");
-        String command = input.next();
-        command = command.toLowerCase();
-        while (!command.equals("y") && !command.equals("n")) {
-            System.out.println("Invalid choice. Please retry.");
-            command = input.next();
-            command = command.toLowerCase();
-        }
-        if (command.equals("y")) {
-            user = new User(username, password);
-            this.loggedIn = true;
         } else {
-            System.out.println("Please re-enter your username and password.");
-            createAccount();
+            System.out.println("Process main menu commands...");
         }
-
-    }
-
-    private void login() {
-        // TODO: implement login
-    }
-
-    private void createListing() {
-        user.createListing();
-    }
-
-    private void viewAllListings() {
-        // TODO: implement viewAllListings
-    }
-
-    private void viewSavedListings() {
-        // TODO: implement viewSavedListings
-    }
-
-    private void viewYourListings() {
-        // TODO: implement viewYourListings
-    }
-
-    private void viewAllBids() {
-
     }
 }
