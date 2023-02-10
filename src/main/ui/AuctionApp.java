65f50098e16bd0d987dca3821ddaa433ee406a89
adrianlam15 <93681064+adrianlam15@users.noreply.github.com>
2023-02-10 10:36:50 -0800

    added create listing method part of UI

--- src/main/ui/AuctionApp.java
+++ src/main/ui/AuctionApp.java
@@ -1,7 +1,9 @@
 package ui;
 
+import model.Car;
 import model.User;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 // Represents the Car Auction App User Interface
@@ -41,9 +43,9 @@ public class AuctionApp {
             System.out.println("\n==== Main menu ====");
             System.out.println("Use numbers to select choices:");
             System.out.println("\t1. Create a listing.");
-            System.out.println("\t4. Place a bid.");
-            System.out.println("\t2. View listings.");
-            System.out.println("\t3. View current bids.");
+            System.out.println("\t2. Place a bid.");
+            System.out.println("\t3. View listings.");
+            System.out.println("\t4. View current bids.");
         }
     }
 
@@ -78,7 +80,57 @@ public class AuctionApp {
                 processCommand(retry);
             }
         } else {
-            System.out.println("Process main menu commands...");
+            if (command.equals("1")) {
+                Car car = new Car();
+                System.out.println("Enter the car's make:");
+                car.setMake(input.next());
+                System.out.println("Enter the car's model:");
+                car.setModel(input.next());
+                System.out.println("Enter the car's colour:");
+                car.setColour(input.next());
+                System.out.println("Enter the car's transmission:");
+                car.setTransmission(input.next());
+                System.out.println("Enter the car's drive type:");
+                car.setDriveType(input.next());
+                System.out.println("Enter the car's condition:");
+                car.setCondition(input.next());
+                System.out.println("Enter the car's year:");
+                car.setYear(input.nextInt());
+                System.out.println("Enter the car's mileage:");
+                car.setMileage(input.nextInt());
+                System.out.println("Enter the car's price:");
+                car.setPrice(input.nextInt());
+                System.out.println("Enter the car's description:");
+                car.setDescription(input.next());
+                System.out.println(car.getMake());
+                System.out.println(car.getModel());
+                System.out.println(car.getColour());
+                System.out.println(car.getTransmission());
+                System.out.println(car.getDriveType());
+                System.out.println(car.getCondition());
+                System.out.println(car.getYear());
+                System.out.println(car.getMileage());
+                System.out.println(car.getPrice());
+                System.out.println(car.getDescription());
+                System.out.println("Is this correct? (y/n):");
+                String confirm = input.next();
+                confirm = confirm.toLowerCase();
+                if (confirm.equals("n")) {
+                    processCommand("1");
+                }
+                user.createCar(car);
+                System.out.println("Listing created successfully.");
+            } else if (command.equals("2")) {
+                user.placeBid();
+            } else if (command.equals("3")) {
+                user.viewListings();
+            } else if (command.equals("4")) {
+                user.viewBids();
+            } else {
+                System.out.println("Invalid entry. Try again:");
+                String retry = input.next();
+                processCommand(retry);
+            }
         }
     }
 }
