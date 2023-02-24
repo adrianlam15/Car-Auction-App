58de47a44a9d753cd5e470abc61527745226a108
adrianlam15 <93681064+adrianlam15@users.noreply.github.com>
2023-02-23 18:09:57 -0800

    moved /test/ to ./src/

--- src/test/CarsTest.java
+++ src/test/CarsTest.java
@@ -1,5 +1,3 @@
-package test;
-
 import model.Car;
 import model.Cars;
 import org.junit.jupiter.api.BeforeEach;
--- src/test/UserTest.java
+++ src/test/UserTest.java
@@ -1,5 +1,3 @@
-package test;
-
 import model.Bid;
 import model.Car;
 import model.Cars;
