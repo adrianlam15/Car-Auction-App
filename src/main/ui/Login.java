6667db6c707acd1733159d60b9c85d98498eaa13
adrianlam15 <93681064+adrianlam15@users.noreply.github.com>
2023-03-17 18:20:54 -0700

    made new JPanel for input fields and labels

--- src/main/ui/Login.java
+++ src/main/ui/Login.java
@@ -1,6 +1,8 @@
 package ui;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -17,26 +19,52 @@ public class Login {
     }
 
     private void initWin() {
-        frame.setLayout(null);
         frame.setSize(width, height);
-        frame.getContentPane().setBackground(new java.awt.Color(15, 23, 42));
-        for (JComponent comp : loadJComp()) {
-            frame.add(comp);
-        }
+        //frame.getContentPane().setBackground(new java.awt.Color(15, 23, 42));
+        frame.add(loadJPanel());
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
-    private List<JComponent> loadJComp() {
-        List<JComponent> comps = new ArrayList<>();
-        comps.addAll(loadButtons());
-        comps.addAll(loadInputFields());
-        return comps;
+    private JPanel loadJPanel() {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(null);
+        mainPanel.setBackground(new java.awt.Color(15, 23, 42));
+        for (JComponent inputField : getInputFields()) {
+            mainPanel.add(inputField);
+        }
+        return mainPanel;
     }
 
-    private Collection<? extends JComponent> loadInputFields() {
-        List<JComponent> inputFields = new ArrayList<>();
+    private ArrayList<JComponent> getInputFields() {
+        Font labelFont = new Font("Roboto", Font.PLAIN, 14);
 
+        ArrayList<JComponent> inputFields = new ArrayList<>();
+        JTextField usernameTextField = new JTextField();
+        JLabel usernameLabel = new JLabel("Username");
+        usernameLabel.setForeground(Color.WHITE);
+        usernameLabel.setLabelFor(usernameTextField);
+        usernameLabel.setFont(labelFont);
+        usernameTextField.setBorder(BorderFactory.create);
 
+        JPasswordField passwordTextField = new JPasswordField();
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setForeground(Color.WHITE);
+        passwordLabel.setLabelFor(passwordTextField);
+        passwordLabel.setFont(labelFont);
+
+        usernameTextField.setBounds((frame.getWidth() - 100) / 2, (frame.getHeight() - 40) / 2 - 50,
+                100, 20);
+        usernameLabel.setBounds((frame.getWidth() - 100) / 2 - 75, (frame.getHeight() - 40) / 2 - 53,
+                100, 20);
+        passwordTextField.setBounds((frame.getWidth() - 100) / 2, (frame.getHeight() - 20) / 2 - 30,
+                100, 20);
+        passwordLabel.setBounds((frame.getWidth() - 100) / 2 - 75, (frame.getHeight() - 20) / 2 - 33,
+                100, 20);
+        inputFields.add(usernameTextField);
+        inputFields.add(passwordTextField);
+        inputFields.add(usernameLabel);
+        inputFields.add(passwordLabel);
         return inputFields;
     }
 
